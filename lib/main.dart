3ae47ef326ed9741d1d94359c21bf019efb2f878
maheshwaran-p <mpmahesh1022@gmail.com>
2023-05-31 17:36:35 +0000

    3 svgs added

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,35 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
-import 'package:samplesvg/svgfile.dart';
+import 'package:samplesvg/svgs/candy.dart';
+import 'package:samplesvg/svgs/caughview.dart';
+import 'package:samplesvg/svgs/gun.dart';
 
 void main() {
-  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: MyApp()));
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'SVG2FLUTTER DEMO',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,61 +38,61 @@ class MyApp extends StatelessWidget {
           color: Colors.white,
           height: MediaQuery.of(context).size.height,
           width: MediaQuery.of(context).size.width,
-          child: Center(
-            //     child: Row(
-            //   // children: [
-            //   //   SvgPicture.asset('asset/key.svg', width: 45, height: 45.2),
-            //   //   SvgPicture.asset('asset/key.svg', width: 45, height: 45.2),
-            //   //   SvgPicture.asset('asset/key.svg', width: 45, height: 45.2),
-            //   //   SvgPicture.asset('asset/key.svg', width: 45, height: 45.2),
-            //   //   SvgPicture.asset('asset/key.svg', width: 45, height: 45.2),
-            //   //   SvgPicture.asset('asset/key.svg', width: 45, height: 45.2),
-            //   //   SvgPicture.asset('asset/key.svg', width: 45, height: 45.2),
-            //   //   SvgPicture.asset('asset/key.svg', width: 45, height: 45.2),
-            //   // ],
-            //   children: [
-            //     CustomPaint(
-            //       painter: CandyPainter(30.0),
-            //       size: Size.zero,
-            //     ),
-            //   ],
-            // )
-            // child: SvgPicture.asset('asset/key.svg', width: 10, height: 10.2),
-
-            //  onTap: () => print('tap'),
-            child: CustomPaint(
-              painter: CandyPainter(40),
-              size: Size.zero,
-            ),
-            // child: Container()
-          )),
+          child: const Center(
+              child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [GunView(), CoughView(), CandyView()]))),
     ));
   }
 }
 
-// class FilledPathPainter extends CustomPainter {
-//   const FilledPathPainter({
-//     @required this.path,
-//     @required this.color,
-//   });
+class GunView extends StatelessWidget {
+  const GunView({super.key});
 
-//   final Path path;
-//   final Color color;
+  static Widget widget(String x) => const GunView();
 
-//   @override
-//   bool shouldRepaint(FilledPathPainter oldDelegate) =>
-//       oldDelegate.path != path || oldDelegate.color != color;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(10.0),
+      padding: const EdgeInsets.all(10.0),
+      decoration: BoxDecoration(
+          color: Colors.white, borderRadius: BorderRadius.circular(20)),
+      child: const GunViewWidget(size: 37), // specify the svg size here .
+    );
+  }
+}
 
-//   @override
-//   void paint(Canvas canvas, Size size) {
-//     canvas.drawPath(
-//       path,
-//       Paint()
-//         ..color = color
-//         ..style = PaintingStyle.fill,
-//     );
-//   }
+class CoughView extends StatelessWidget {
+  const CoughView({super.key});
 
-//   @override
-//   bool hitTest(Offset position) => path.contains(position);
-// }
+  static Widget widget(String x) => const CoughView();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(10.0),
+      padding: const EdgeInsets.all(10.0),
+      decoration: BoxDecoration(
+          color: Colors.white, borderRadius: BorderRadius.circular(20)),
+      child: const CoughWidget(size: 42),
+    );
+  }
+}
+
+class CandyView extends StatelessWidget {
+  const CandyView({super.key});
+
+  static Widget widget(String x) => const CandyView();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(10.0),
+      padding: const EdgeInsets.all(10.0),
+      decoration: BoxDecoration(
+          color: Colors.white, borderRadius: BorderRadius.circular(20)),
+      child: const CandyWidget(size: 47),
+    );
+  }
+}
